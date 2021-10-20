881626776042e490e5c053cd52ce0d7fd18a050d
Bartosz Kucharz <bartosz_kucharz@outlook.com>
2021-10-20 20:53:07 +0200

    Done every task

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,23 +1,46 @@
 package agh.ics.oop;
 
+import static agh.ics.oop.Direction.*;
+
 public class World {
-    public static void run(String[] arguments){
-        System.out.println("Zwierzak idzie do przodu");
-        String message = "";
-        for (String argument : arguments){
-            message = message + argument + ", ";
+    public static void run(Direction[] arguments){
+        System.out.println("Start");
+
+        for (Direction dir : arguments){
+            if (dir == null){
+                continue;
+            }
+            String message = switch (dir){
+                case FORWARD -> "idzie do przodu";
+                case BACKWARD -> "idzie do tyłu";
+                case RIGHT -> "skręca w prawo";
+                case LEFT -> "skręca w lewo";
+            };
+            System.out.println("Zwierzak " + message);
         }
-        message.replaceAll(".$", "");
-        message.replaceAll(".$", "");
-        System.out.println(message);
+        System.out.println("Stop");
     }
 
+    public static Direction[] convertToEnum(String[] strings){
+        int n = strings.length;
+        Direction[] converted = new Direction[n];
+        for(int i = 0; i < n; i++){
+            converted[i] = switch(strings[i]){
+                case "f" -> FORWARD;
+                case "b" -> BACKWARD;
+                case "r" -> RIGHT;
+                case "l" -> LEFT;
+                default -> null;
+            };
+        }
+        return converted;
+    }
 
     public static void main(String[] args){
-        System.out.println("System wystartowal");
-        String[] strArray = new String[]{"Foo","Bar","Baz"};
-        run(strArray);
-        System.out.println("System zakonczyl dzialanie");
+        System.out.println("System wystartował");
+        Direction[] directions = convertToEnum(args);
+        run(directions);
+        System.out.println("System zakonczył działanie");
 
     }
 }
